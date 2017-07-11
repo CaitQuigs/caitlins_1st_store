14fc26bfb3181c457885e8112b079fa70729c4f7
Caitlin Quigley <cquigley13@gmail.com>
2017-07-11 15:27:59 +0000

    end of day 1 class

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-	root 'products#index'
+  root 'storefront#all_items'
+
+  get 'categories' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
